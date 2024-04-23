2614c2ba6301d23fffb17f1eb324bcfae8cfdd83
Лиен <blelik3346020@gmail.com>
2024-04-23 14:19:36 +0300

    Update rk_name_budget.sql

--- Marketing Budget /rk_name_budget.sql
+++ Marketing Budget /rk_name_budget.sql
@@ -1,20 +1,21 @@
 SELECT name, budget
-from (SELECT *
-	  from users 
-	  where owner_id NOT IN (select id from users u)
-	  or owner_id is NULL) t5
-left JOIN (select comp_id, sum(user_sum) as budget
-		   from (select COALESCE (owner_id,id) as comp_id, user_sum  
-		   		 from users u
-		   		 join (select user_id, sum(sum) as user_sum 
-		   		 	   from payment_date
-		   		 	   where sum >0
-		   		 	   and rk_id in (select id as rk_id from (select * from advertising_companies ac
-		   		 	   				 	   			 where is_main = 1
-		   		 	   				 	             OR create_date = (
-		   		 	   				 	             		select min(create_date) from advertising_companies ac)))
-		   		 	  group by user_id) t2 
-		   		 on t2.user_id = u.id) t3
-		   		 group by comp_id) t4 
-on t5.id = t4.comp_id
-order by name
+FROM
+  (SELECT *
+   FROM users
+   WHERE owner_id NOT IN (SELECT id FROM users u)
+   OR owner_id IS NULL) t5
+LEFT JOIN
+  (SELECT comp_id, sum(user_sum) AS budget
+   FROM (SELECT COALESCE (owner_id, id) AS comp_id, user_sum
+         FROM users u
+         JOIN
+        (SELECT user_id, sum(SUM) AS user_sum
+         FROM payment_date
+         WHERE SUM >0
+         AND rk_id in (SELECT id AS rk_id FROM (SELECT *
+						  FROM advertising_companies ac
+						  WHERE is_main = 1
+						  OR create_date = (SELECT min(create_date) FROM advertising_companies ac)))
+         GROUP BY user_id) t2 ON t2.user_id = u.id) t3
+    GROUP BY comp_id) t4 ON t5.id = t4.comp_id
+ORDER BY name
